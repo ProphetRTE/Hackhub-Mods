c2684c47766576d13b1cf8c6137d968e977b0ff4
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2026-06-01 17:13:18 -0400

    Update ExampleCommand.ts

--- cant-hack-this/src/commands/ExampleCommand.ts
+++ cant-hack-this/src/commands/ExampleCommand.ts
@@ -6,6 +6,11 @@ export class IpGenCommand extends Command {
     CommandName = "example";
     Description = "An example command to build a new one off of.";
 
+    Autocomplete = [
+        { label: "example", type: "STRING" as const },
+      //{ label: "<count>", type: "NUMBER" as const },
+    ];
+
     Run(tools: CommandTools): Promise<void> | void {
         tools.printInfo("Hello World!");
     }
